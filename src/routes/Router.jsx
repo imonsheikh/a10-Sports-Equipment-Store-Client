504cd2d8d02f9642aae0d781e8a6c197e5079c19
imon-sheikh <mdimonhossain2017@gmail.com>
2024-12-08 08:55:01 +0600

    added error page and router link up with home page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,16 +4,28 @@ import {
   } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home";
+import AllEquipment from "../pages/AllEquipment";
+import AddEquipment from "../pages/AddEquipment";
+import ErrorPage from "../pages/ErrorPage";
 
   
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+            path: '/allEquipment',
+            element: <AllEquipment></AllEquipment>
+        },
+        {
+            path: 'addEquipment',
+            element: <AddEquipment></AddEquipment>
         }
       ]
     },
